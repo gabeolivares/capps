0bb5f0dd9f00c78051ea6b9df9d609f4a19bb1b7
Gabe Olivares <gabeolivares60@gmail.com>
2018-08-30 17:57:25 -0500

    Change all sports.

--- app/controllers/all_sports_controller.rb
+++ app/controllers/all_sports_controller.rb
@@ -4,6 +4,7 @@ class AllSportsController < ApplicationController
   end
   def show
     @sport = Sport.find(params[:id])
-    @game = Game.where(sport: params[:id]).order(:time)
+    @game = Game.where(sport: params[:id]).order(location: :desc,time: :asc)
+    #@game = Game.where(sport: params[:id]).order(:time)
   end
 end
