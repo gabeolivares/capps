4342253ade13318e13c4580d56c557b827729431
Gabe Olivares <gabeolivares60@gmail.com>
2018-09-06 22:37:31 -0500

    Fix date time in all sport

--- app/controllers/all_sports_controller.rb
+++ app/controllers/all_sports_controller.rb
@@ -4,7 +4,13 @@ class AllSportsController < ApplicationController
   end
   def show
     @sport = Sport.find(params[:id])
-    @game = Game.where(sport: params[:id]).order(time: :asc,location: :asc)
+    p "hereeeeeeeeeeee"
+    @game = Game.where(sport: params[:id]).sort_by{|ev| [ev.time.strftime('%m/%d/%Y'),ev.location,ev.time.strftime('%H:%M')]}
+
+    #p @game.sort_by{|ev| ev.time.strftime('%H:%M')}
+
+    #@game = Game.where(sport: params[:id]).order(time: :asc, location: :asc)
+    #@game.sort_by{|ev| ev.time.strftime('%H:%M')}
     #@game = Game.where(sport: params[:id]).order(:time)
   end
 end
