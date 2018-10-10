6742981192e55d9c971c1e7dc94c9490366b5c9b
Gabe Olivares <gabeolivares60@gmail.com>
2018-10-10 16:30:40 -0500

    Required for tourney game. Not require.

--- app/controllers/sport_controller.rb
+++ app/controllers/sport_controller.rb
@@ -47,25 +47,21 @@ class SportController < ApplicationController
     game_id = params[:id]
     @division = Division.find_by(name: div_name, grade: div_grade)
 
-    unless (params['game']['time(1i)'] == "" || params['game']['time(2i)'] == "" || params['game']['time(3i)'] == "")
-      game = game_update_param
-      game[:division] = @division.id
-      game[:sport] = sport_id
 
-      @game = Game.find(game_id)
+    game = game_update_param
+    game[:division] = @division.id
+    game[:sport] = sport_id
 
-      if @game.update_attributes(game)
-         redirect_to :action => 'show', :id => sport_id, :division_id => sport_id, :division_grade => div_grade, :division_name => div_name
-      else
-         @game = Game.all
-         flash[:error] = "We were unable to update the game. Please make sure all fields are filled."
-         render :action => 'show', :id => sport_id, :division_id => sport_id, :division_grade => div_grade, :division_name => div_name
-      end
+    @game = Game.find(game_id)
+
+    if @game.update_attributes(game)
+       redirect_to :action => 'show', :id => sport_id, :division_id => sport_id, :division_grade => div_grade, :division_name => div_name
     else
-
-      flash[:error] = "We were unable to update the game. Please make sure all fields are filled."
-      redirect_to :back
+       @game = Game.all
+       flash[:error] = "We were unable to update the game. Please make sure all fields are filled."
+       render :action => 'show', :id => sport_id, :division_id => sport_id, :division_grade => div_grade, :division_name => div_name
     end
+
   end
 
   def show
